91ba33b1c48bc9b3c90ff90edb1067edf988bf3a
Tobias Jonas <t.jonas@innfactory.de>
2018-12-17 13:09:20 +0100

    LoadBalancer Service

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,6 +3,6 @@
 rp generate-kubernetes-resources "eu.gcr.io/innfactory-k8s101/k8sdemo:latest" \
   --generate-all \
   --ingress-name k8sdemo \
-  --service-type NodePort \
+  --service-type LoadBalancer \
   --version 1.0 \
   --pod-controller-replicas 3 | kubectl apply -f -
